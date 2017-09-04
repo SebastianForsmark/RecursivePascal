44360004fe9686747778638536858e00c38a04c8
SebastianForsmark <sebastian.forsmark@gmail.com>
2017-09-04 20:36:41 +0200

    1

---
+++ src/main/java/se/kth/ID1020/App.java
@@ -0,0 +1,13 @@
+package se.kth.ID1020;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    RecursivePascal test = new RecursivePascal();
+     test.printPascal(40);
+    }
+}
